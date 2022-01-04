c756e3a46e7cb30aabf0be9fd0f760eace229c7b
Tom Brennan <tjb1982@gmail.com>
2022-01-04 14:07:58 -0500

    use testnet directly

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,5 +1,6 @@
 const appName = "Imbue Network";
-const webSocketAddr = "ws://127.0.0.1:8081/wsapp/";
+// const webSocketAddr = "ws://127.0.0.1:8081/wsapp/";
+const webSocketAddr = "wss://dev.imbue.network";
 // http://3.9.171.186:3000/?rpc=ws%3A%2F%2F3.9.171.186%3A9942#/chainstate
 // const webSocketAddr = "ws://3.9.171.186:9944"
 

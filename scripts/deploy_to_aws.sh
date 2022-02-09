b412c0db9bd1f0bf4ea5080399aa89c0f75c4bf5
Sam Elamin <hussam.elamin@gmail.com>
2022-02-09 15:38:46 +0000

    deploy to vanilla ubuntu instance (#19)

--- scripts/deploy_to_aws.sh
+++ scripts/deploy_to_aws.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
- 
 
+#AMI id ami-03a6074373e014172
 GOOGLE_OAUTH2_CLIENT_ID=$1
 GOOGLE_OAUTH2_CLIENT_SECRET=$2
 BRANCH_NAME=$3
 echo 'Starting to Deploy...'
 echo 'BRANCH NAME IS ' $BRANCH_NAME
 
-ssh ec2-user@3.12.198.64 -o StrictHostKeyChecking=no " 
-
+ssh ec2-user@18.191.252.212 -o StrictHostKeyChecking=no " 
         export GOOGLE_OAUTH2_CLIENT_ID=$GOOGLE_OAUTH2_CLIENT_ID;
         export GOOGLE_OAUTH2_CLIENT_SECRET=$GOOGLE_OAUTH2_CLIENT_SECRET;
         export BRANCH_NAME=$BRANCH_NAME;

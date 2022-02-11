c4687c1d65a4324434678ae519c88afb7da3aaec
sam elamin <hussam.elamin@gmail.com>
2022-02-11 10:27:24 +0000

    force removal of volumes

--- scripts/deploy_to_aws.sh
+++ scripts/deploy_to_aws.sh
@@ -7,13 +7,13 @@ BRANCH_NAME=$3
 echo 'Starting to Deploy...'
 echo 'BRANCH NAME IS ' $BRANCH_NAME
 
-ssh ec2-user@18.191.252.212 -o StrictHostKeyChecking=no " 
+ssh ec2-user@3.144.183.82 -o StrictHostKeyChecking=no " 
         export GOOGLE_OAUTH2_CLIENT_ID=$GOOGLE_OAUTH2_CLIENT_ID;
         export GOOGLE_OAUTH2_CLIENT_SECRET=$GOOGLE_OAUTH2_CLIENT_SECRET;
         export BRANCH_NAME=$BRANCH_NAME;
         rm -rf dapp && git clone https://github.com/ImbueNetwork/dapp/ && cd dapp; git checkout $BRANCH_NAME;
         docker-compose -f docker-compose.staging.yml down;
-        docker system prune --volumes;
+        docker system prune --volumes -f;
         docker-compose -f docker-compose.staging.yml build --no-cache; 
         docker-compose -f docker-compose.staging.yml up -d;
         docker exec api make db_up;

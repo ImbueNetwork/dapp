8073da6a1d8d5b1ba90d560ddcc275f12f056d92
sam elamin <hussam.elamin@gmail.com>
2022-05-02 09:02:30 +0100

    sudo remove folder

--- scripts/deploy_to_aws.sh
+++ scripts/deploy_to_aws.sh
@@ -15,7 +15,7 @@ ssh ec2-user@3.144.183.82 -o StrictHostKeyChecking=no "
         export GOOGLE_OAUTH2_CLIENT_ID=$GOOGLE_OAUTH2_CLIENT_ID;
         export GOOGLE_OAUTH2_CLIENT_SECRET=$GOOGLE_OAUTH2_CLIENT_SECRET;
         export BRANCH_NAME=$BRANCH_NAME;
-        rm -rf dapp && git clone https://github.com/ImbueNetwork/dapp/ && cd dapp; git checkout $BRANCH_NAME;
+        sudo rm -rf dapp && git clone https://github.com/ImbueNetwork/dapp/ && cd dapp; git checkout $BRANCH_NAME;
         docker-compose -f docker-compose.staging.yml down;
         docker system prune --volumes -f;
         docker-compose -f docker-compose.staging.yml build --no-cache; 

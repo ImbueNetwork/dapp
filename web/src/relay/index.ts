dbc45a728e10cb8912253aec563bdf83f30cbf21
sam elamin <hussam.elamin@gmail.com>
2022-08-05 18:17:25 +0100

    add error message if transferring over available balance

--- web/src/relay/index.ts
+++ web/src/relay/index.ts
@@ -61,10 +61,9 @@ export default class Relay extends HTMLElement {
     ): Promise<void> {
         const relayApi = this.apiInfo?.relayChain?.api;
         const imbueApi = this.apiInfo?.imbue?.api;
+        const transferAmount = BigInt(parseFloat(this.$transferAmount.value as string) * 1e12);
 
-        const amount = parseInt(this.$transferAmount.value as string) * 1e12;
-
-        if (relayApi && amount > 0) {
+        if (relayApi && transferAmount > 0) {
 
             this.$transfer.disabled = true;
             this.$transfer.classList.add("blob");
@@ -78,64 +77,74 @@ export default class Relay extends HTMLElement {
                     detail: {
                         callback: async (account?: InjectedAccountWithMeta) => {
                             if (account) {
-                                const dest = {V0: {X1: {Parachain: 2121}}};
-
-                                const beneficiary = {
-                                    V1: {
-                                        parents: 0,
-                                        interior: {
-                                            X1: {
-                                                AccountId32: {
-                                                    network: "Any",
-                                                    id: decodeAddress(account.address)
+                                const { data: { free: freeBalance } } = await relayApi.query.system.account(account.address);
+                                const userHasEnoughBalance = freeBalance.toBigInt() >= transferAmount
+                                if (userHasEnoughBalance) {
+                                    const dest = { V0: { X1: { Parachain: 2121 } } };
+                                    const beneficiary = {
+                                        V1: {
+                                            parents: 0,
+                                            interior: {
+                                                X1: {
+                                                    AccountId32: {
+                                                        network: "Any",
+                                                        id: decodeAddress(account.address)
+                                                    }
                                                 }
                                             }
                                         }
-                                    }
-                                };
+                                    };
 
-                                const assets = {
-                                    V1: [{
-                                        id: {Concrete: {parents: 0, interior: "Here"}},
-                                        fun: {Fungible: amount}
-                                    }]
-                                };
+                                    const assets = {
+                                        V1: [{
+                                            id: { Concrete: { parents: 0, interior: "Here" } },
+                                            fun: { Fungible: transferAmount }
+                                        }]
+                                    };
 
-                                const feeAssetItem = 0;
+                                    const feeAssetItem = 0;
 
-                                const injector = await web3FromSource(account.meta.source);
-                                const extrinsic = relayApi?.tx.xcmPallet.reserveTransferAssets(dest, beneficiary, assets, feeAssetItem);
+                                    const injector = await web3FromSource(account.meta.source);
+                                    const extrinsic = relayApi?.tx.xcmPallet.reserveTransferAssets(dest, beneficiary, assets, feeAssetItem);
 
-                                try {
-                                    const txHash = await extrinsic.signAndSend(
-                                        account.address,
-                                        {signer: injector.signer},
-                                        ({status}) => {
-                                            imbueApi?.query.system.events((events: any) => {
-                                                if (events) {
-                                                    // Loop through the Vec<EventRecord>
-                                                    events.forEach((record: any) => {
-                                                        const {event, phase} = record;
-                                                        const currenciesDeposited = `${event.section}.${event.method}` == "ormlTokens.Deposited";
-                                                        if (currenciesDeposited) {
-                                                            const types = event.typeDef;
-                                                            const accountId = event.data[1];
+                                    try {
+                                        const txHash = await extrinsic.signAndSend(
+                                            account.address,
+                                            { signer: injector.signer },
+                                            ({ status }) => {
+                                                imbueApi?.query.system.events((events: any) => {
+                                                    if (events) {
+                                                        // Loop through the Vec<EventRecord>
+                                                        events.forEach((record: any) => {
+                                                            const { event, phase } = record;
+                                                            const currenciesDeposited = `${event.section}.${event.method}` == "ormlTokens.Deposited";
+                                                            if (currenciesDeposited) {
+                                                                const types = event.typeDef;
+                                                                const accountId = event.data[1];
 
-                                                            if (accountId == account.address) {
-                                                                this.$transfer.classList.remove("blob");
-                                                                this.$transfer.disabled = false;
-                                                                this.$transfer.classList.add("finalized");
-                                                                this.$transfer.innerText = "Transfer Succeeded";
+                                                                if (accountId == account.address) {
+                                                                    this.$transfer.classList.remove("blob");
+                                                                    this.$transfer.disabled = false;
+                                                                    this.$transfer.classList.add("finalized");
+                                                                    this.$transfer.innerText = "Transfer Succeeded";
+                                                                }
                                                             }
-                                                        }
-                                                    });
-                                                }
+                                                        });
+                                                    }
+                                                });
                                             });
-                                        });
-                                } catch (error: any) {
-                                    this.errorNotification(error);
-                                    this.$transfer.classList.remove("blob");
+                                    } catch (error: any) {
+                                        this.errorNotification(error);
+                                        this.$transfer.classList.remove("blob");
+                                        this.$transfer.disabled = false;
+                                        this.$transfer.innerText = "Transfer";
+                                    }
+                                } else {
+                                    const errorMessage = `Error: Insuffient balance to complete transfer. Available balance is ${(freeBalance / 1e12).toFixed(2)}`
+                                    this.errorNotification(Error(errorMessage));
+
                                     this.$transfer.disabled = false;
+                                    this.$transfer.classList.remove("blob");
                                     this.$transfer.innerText = "Transfer";
                                 }
                             }
@@ -150,7 +159,7 @@ export default class Relay extends HTMLElement {
         this.apiInfo = await request.apiInfo;
         return;
     }
-    
+
     errorNotification(e: Error) {
         console.log(e);
         this.dispatchEvent(new CustomEvent(

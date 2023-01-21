67ba26f3f271f8631bf9b6d3a30015d95e51393e
cuteolaf <oliverlim818@gmail.com>
2023-01-21 09:37:51 -0800

    css update: registration form

--- api/src/frontend/join.tsx
+++ api/src/frontend/join.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import { useRef, useState, useEffect } from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { TextField } from "@rmwc/textfield";
-import '@rmwc/textfield/styles';
-import "../../public/registration.css"
-import bcrypt from 'bcryptjs'
+import "@rmwc/textfield/styles";
+import "../../public/registration.css";
+import bcrypt from "bcryptjs";
 import * as config from "./config";
-import * as utils from "./utils"
+import * as utils from "./utils";
 
 const getAPIHeaders = {
     accept: "application/json",
@@ -18,13 +18,13 @@ const postAPIHeaders = {
 };
 
 function Join() {
-    const [user, setUser] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [matchPassword, setMatchPassword] = useState('');
-    const [error, setError] = useState('');
+    const [user, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [matchPassword, setMatchPassword] = useState("");
+    const [error, setError] = useState("");
 
-    const salt = bcrypt.genSaltSync(10)
+    const salt = bcrypt.genSaltSync(10);
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
@@ -33,7 +33,7 @@ function Join() {
         var body = {
             username: user,
             email: email,
-            password: hashedPassword
+            password: hashedPassword,
         };
 
         const resp = await fetch(`/auth/imbue/register`, {
@@ -48,15 +48,20 @@ function Join() {
             const error = await resp.json();
             setError(error);
         }
-    }
+    };
 
     return (
-
-        <form id="contribution-submission-form" name="contribution-submission-form" method="get" onSubmit={handleSubmit}>
-            <div id="registration-form" className='registration-container'>
-
+        <form
+            id="contribution-submission-form"
+            name="contribution-submission-form"
+            method="get"
+            onSubmit={handleSubmit}
+        >
+            <div id="registration-form" className="registration-container">
                 <div className="contents">
-                    <div><h1>Sign up</h1></div>
+                    <div>
+                        <h1>Sign up</h1>
+                    </div>
 
                     <div>
                         <TextField
@@ -65,7 +70,10 @@ function Join() {
                             type="text"
                             pattern="^[A-z][A-z0-9-_]{3,23}$"
                             label="Username"
-                            outlined className="mdc-text-field" required />
+                            outlined
+                            className="mdc-text-field"
+                            required
+                        />
                     </div>
 
                     <div>
@@ -73,7 +81,10 @@ function Join() {
                             type="email"
                             label="Email"
                             onChange={(e: any) => setEmail(e.target.value)}
-                            outlined className="mdc-text-field" required />
+                            outlined
+                            className="mdc-text-field"
+                            required
+                        />
                     </div>
                     <div>
                         <TextField
@@ -82,27 +93,45 @@ function Join() {
                             onChange={(e: any) => setPassword(e.target.value)}
                             pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
                             type="password"
-                            outlined className="mdc-text-field" required />
+                            outlined
+                            className="mdc-text-field"
+                            required
+                        />
                     </div>
                     <div>
                         <TextField
                             label="Confirm Password"
-                            invalid={matchPassword.length > 0 && password != matchPassword}
+                            invalid={
+                                matchPassword.length > 0 &&
+                                password != matchPassword
+                            }
                             pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                            onChange={(e: any) => setMatchPassword(e.target.value)}
-                            helpText={password != matchPassword ? "Please match password" : ""}
+                            onChange={(e: any) =>
+                                setMatchPassword(e.target.value)
+                            }
+                            helpText={
+                                password != matchPassword
+                                    ? "Please match password"
+                                    : ""
+                            }
                             type="password"
-                            outlined className="mdc-text-field" required />
+                            outlined
+                            className="mdc-text-field"
+                            required
+                        />
                     </div>
                     <div>
-                        <span className={!error ? "hide" : "error"}>{error}</span>
+                        <span className={!error ? "hide" : "error"}>
+                            {error}
+                        </span>
                     </div>
                     <div>
                         <button
                             type="submit"
                             disabled={password != matchPassword}
                             className="primary-btn in-dark confirm"
-                            id="create-account">
+                            id="create-account"
+                        >
                             Create my account
                         </button>
                     </div>

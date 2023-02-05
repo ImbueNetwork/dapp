1d25ab0906d0841917914839472d104df62d080f
gatsey <felixgate13@gmail.com>
2023-02-05 18:42:17 +0000

    gotta move middle of debug :/

--- api/src/backend/db/seeds/202301311300_freelancers.ts
+++ api/src/backend/db/seeds/202301311300_freelancers.ts
@@ -1,10 +1,6 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
-    await generate_freelancer(knex);
-};
-
-async function generate_freelancer(knex: Knex): Promise<void> {
     let skills = [
         "substrate",
         "rust",
@@ -16,23 +12,6 @@ async function generate_freelancer(knex: Knex): Promise<void> {
         "figma",
         "adobe",
     ]
-
-    let industries = [
-        "web3",
-        "defi",
-        "education",
-        "agriculture",
-        "communications",
-        "health",
-        "wellness",
-        "energy",
-        "sustainability",
-        "arts",
-        "real",
-        "technology",
-        "supply",
-    ]
-
     let services = [
         "web development",
         "web design",
@@ -43,14 +22,12 @@ async function generate_freelancer(knex: Knex): Promise<void> {
         "video editing",
         "nft",
     ]
-
     let clients = [
         "imbue",
         "mangata",
         "oak",
         "nft",
     ]
-
     let languages = [
         "french",
         "german",
@@ -62,66 +39,66 @@ async function generate_freelancer(knex: Knex): Promise<void> {
     ]
 
     let c = 4;
-    skills.map(async(skill, a) => {
-            clients.map(async (client, b) => {
-                languages.map(async (lang, c) => {
-                    services.map(async (service, d) => {
-
+    for (let a = 1; a == skills.length; a++) {
+        for (let b = 1; b == clients.length; b++) {
+            for (let c = 1; c == languages.length; c++) {
+                for (let d = 1; d == services.length; d++) {
                     c = c + 1;
                     let rsmall =  Math.floor(Math.random() * (3 - 1 + 1)) + 1;
                     let rbig =  Math.floor(Math.random() * (100 - 1 + 1)) + 1;
                     // new user associated
-                    await knex("users").insert([
+                    await knex("users").insert(
                         {
-                            display_name: skill + " pro" + rsmall.toExponential(rbig),
-                            username: skill+lang,
-                            email: skill+lang+"industry@gmail.com",
+                            display_name: skills[a] + " pro" + rsmall.toExponential(rbig),
+                            username: skills[a]+languages[c],
+                            email: skills[a]+languages[c]+"industry@gmail.com",
                             password: "testpassword",
                         }
-                    ]).then(async () => {
-                        await knex("freelancers").insert({
+                     )
+                    //.then(async () => {
+                    //     await knex("freelancers").insert({
                         
-                            freelanced_before: "I've freelanced before however, i may need some extra help.",
-                            freelancing_goal: "To make a little extra money on the side",
-                            title: "A " + lang + " speaking" + skill  + " professional",
-                            bio: "I also have experience in " + service + " and have many clients including " + client,
-                            work_type: "To make a little extra money on the side",
-                            facebook_link: "www.facebook.com/pro" + lang + skill,
-                            twitter_link: "www.twitter.com/pro" + lang + skill,
-                            telegram_link: "www.telegram.com/pro" + lang + skill,
-                            discord_link: "www.discord.com/pro" + lang + skill,
-                            user_id: c,
-                        }).then(async () => {
+                    //         freelanced_before: "I've freelanced before however, i may need some extra help.",
+                    //         freelancing_goal: "To make a little extra money on the side",
+                    //         title: "A " + languages[c] + " speaking" + skills[a]  + " professional",
+                    //         bio: "I also have experience in " + services[d] + " and have many clients including " + clients[b],
+                    //         work_type: "To make a little extra money on the side",
+                    //         facebook_link: "www.facebook.com/pro" + languages[c] + skills[a],
+                    //         twitter_link: "www.twitter.com/pro" + languages[c] + skills[a],
+                    //         telegram_link: "www.telegram.com/pro" + languages[c] + skills[a],
+                    //         discord_link: "www.discord.com/pro" + languages[c] + skills[a],
+                    //         user_id: c,
+                    //     }).then(async () => {
                             
-                            for (let i = 0; i < rsmall; i++) {
+                    //         for (let i = 0; i < rsmall; i++) {
                             
-                                if (i == 0) {
-                                    await knex("freelancer_skills").insert({ freelancer_id: c - 3, skill_id: a})
-                                    await knex("freelancer_clients").insert({freelancer_id: c - 3, client_id: b})
-                                    await knex("freelancer_services").insert({freelancer_id: c - 3, language_id: c})
-                                    await knex("freelancer_languages").insert({freelancer_id: c - 3, service_id: a})
-                                }
+                    //             if (i == 0) {
+                    //                 await knex("freelancer_skills").insert({ freelancer_id: c - 3, skill_id: a})
+                    //                 await knex("freelancer_clients").insert({freelancer_id: c - 3, client_id: b})
+                    //                 await knex("freelancer_languages").insert({freelancer_id: c - 3, service_id: c})
+                    //                 await knex("freelancer_services").insert({freelancer_id: c - 3, language_id: d})
+                    //             }
 
-                                if (i != a) {
-                                    await knex("freelancer_skills").insert({ freelancer_id: c - 3, skill_id: i})
-                                }
-                                if (i != b) {
-                                    await knex("freelancer_clients").insert({ freelancer_id: c - 3, client_id: i})
-                                }
-                                if (i != c) {
-                                    await knex("freelancer_services").insert({ freelancer_id: c - 3, service_id: i})
-                                }
-                                if (i != b) {
-                                    await knex("freelancer_languages").insert({ freelancer_id: c - 3, language_id: i})
-                                } 
-                        } 
+                    //             if (i != a) {
+                    //                 await knex("freelancer_skills").insert({ freelancer_id: c - 3, skill_id: i})
+                    //             }
+                    //             if (i != b) {
+                    //                 await knex("freelancer_clients").insert({ freelancer_id: c - 3, client_id: i})
+                    //             }
+                    //             if (i != c) {
+                    //                 await knex("freelancer_languages").insert({ freelancer_id: c - 3, language_id: i})
+                    //             } 
+                    //             if (i != d) {
+                    //                 await knex("freelancer_services").insert({ freelancer_id: c - 3, service_id: i})
+                    //             }
+                    //     } 
 
-                        })
-                    });
-                })
-            })
-        })
-    })
+                    //     })
+                    // })
+                }
+            }
+        }
+    }
 
-}
+};
    

b98bb997c141dbc016b5fbf20cd7c6a99733c864
cuteolaf <oliverlim818@gmail.com>
2023-02-07 15:25:05 -0800

    errorDialog

--- api/src/frontend/components/errorDialog.tsx
+++ api/src/frontend/components/errorDialog.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { ReactElement } from "react";
 import { Dialog, DialogActions, DialogButton, DialogContent, DialogTitle } from "@rmwc/dialog";
-import { Button } from "@rmwc/button";
 
 
 export type ErrorDialogProps = {
@@ -10,26 +8,20 @@ export type ErrorDialogProps = {
     closeDialog: () => void
 }
 
-export class ErrorDialog extends React.Component<ErrorDialogProps> {
-    constructor(props: ErrorDialogProps) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Dialog
-                open={this.props.showDialog}
-                onClose={(evt) => {
-                    this.props.closeDialog();
-                }}>
-                <DialogTitle>Error</DialogTitle>
-                <DialogContent>{this.props.errorMessage}</DialogContent>
-                <DialogActions>
-                    <DialogButton action="accept" isDefaultAction>
-                        Ok
-                    </DialogButton>
-                </DialogActions>
-            </Dialog>
-        );
-    }
+export const ErrorDialog = ({ errorMessage, showDialog, closeDialog }: ErrorDialogProps): JSX.Element => {
+    return (
+        <Dialog
+            open={showDialog}
+            onClose={(evt) => {
+                closeDialog();
+            }}>
+            <DialogTitle>Error</DialogTitle>
+            <DialogContent>{errorMessage}</DialogContent>
+            <DialogActions>
+                <DialogButton action="accept" isDefaultAction>
+                    Ok
+                </DialogButton>
+            </DialogActions>
+        </Dialog>
+    );
 }

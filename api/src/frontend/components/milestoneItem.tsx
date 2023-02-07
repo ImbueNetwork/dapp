ed5899838b3433308620d06e3e401d66c198650c
cuteolaf <oliverlim818@gmail.com>
2023-02-07 15:36:05 -0800

    milestoneItem

--- api/src/frontend/components/milestoneItem.tsx
+++ api/src/frontend/components/milestoneItem.tsx
@@ -1,6 +1,6 @@
-  import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
-import {  BsCircleFill } from "react-icons/bs";
+import { BsCircleFill } from "react-icons/bs";
 import { Currency, Milestone, ProjectOnChain, ProjectState } from "../models";
 
 
@@ -12,43 +12,35 @@ export type MilestonesItemProps = {
   toggleActive: boolean
 }
 
-type MilestonesState = {
-  formattedMilestoneValue: string
-}
+const MilestoneItem = ({ milestone, projectOnChain, toggleActive, toggleMilestone, isInVotingRound }: MilestonesItemProps): JSX.Element => {
 
-class MilestoneItem extends React.Component<MilestonesItemProps, MilestonesState> {
+  const [formattedMilestoneValue, setFormattedMilestoneValue] = useState("0");
 
-  state: MilestonesState = {
-    formattedMilestoneValue: "0"
-  }
-
-  componentDidMount() {
-    if (this.props.milestone) {
-      const milestoneValue = Number((this.props.milestone.percentageToUnlock / 100) * this.props.projectOnChain.requiredFundsFormatted);
-      this.setState({
-        formattedMilestoneValue: milestoneValue.toLocaleString()
-      });
+  useEffect(() => {
+    if (milestone) {
+      const milestoneValue = Number((milestone.percentageToUnlock / 100) * projectOnChain.requiredFundsFormatted);
+      setFormattedMilestoneValue(milestoneValue.toLocaleString());
     }
-  }
+  }, []);
 
 
 
-  displayMilestoneToggle(): JSX.Element {
-    if (this.props.milestone.isApproved) {
-      if (this.props.toggleActive) {
-        return <div> <BsCircleFill className="milestone-status-icon"/>  <span className="milestone-status approved"> Approved </span>  <AiFillMinusCircle /></div>
+  const displayMilestoneToggle = (): JSX.Element => {
+    if (milestone.isApproved) {
+      if (toggleActive) {
+        return <div> <BsCircleFill className="milestone-status-icon" />  <span className="milestone-status approved"> Approved </span>  <AiFillMinusCircle /></div>
       } else {
-        return <div> <BsCircleFill className="milestone-status-icon"/>  <span className="milestone-status approved"> Approved </span> <AiFillPlusCircle /></div>
+        return <div> <BsCircleFill className="milestone-status-icon" />  <span className="milestone-status approved"> Approved </span> <AiFillPlusCircle /></div>
       }
-    } else if (this.props.isInVotingRound) {
-      if (this.props.toggleActive) {
-        return <div> <BsCircleFill className="milestone-status-icon active"/>  <span className="milestone-status active"> Active </span>  <AiFillMinusCircle /></div>
+    } else if (isInVotingRound) {
+      if (toggleActive) {
+        return <div> <BsCircleFill className="milestone-status-icon active" />  <span className="milestone-status active"> Active </span>  <AiFillMinusCircle /></div>
       } else {
-        return <div> <BsCircleFill className="milestone-status-icon active"/>  <span className="milestone-status active"> Active </span> <AiFillPlusCircle /></div>
+        return <div> <BsCircleFill className="milestone-status-icon active" />  <span className="milestone-status active"> Active </span> <AiFillPlusCircle /></div>
       }
 
     } else {
-      if (this.props.toggleActive) {
+      if (toggleActive) {
         return <div> <span className="milestone-status not-started">Not Started </span> <AiFillMinusCircle /></div>
       } else {
         return <div> <span className="milestone-status not-started">Not Started  </span><AiFillPlusCircle /></div>
@@ -57,35 +49,33 @@ class MilestoneItem extends React.Component<MilestonesItemProps, MilestonesState
   }
 
 
-  render() {
-    return (
-      <div className="main">
-        <div
-          className="milestone-row"
-          onClick={() => {
-            this.props.toggleMilestone(this.props.milestone.milestoneKey);
-          }}
-        >
-          <span>{this.props.milestone.name}</span>
-          {this.displayMilestoneToggle()}
-        </div>
-        <div className={this.props.toggleActive ? "content show" : "content hide"}>
-          <div className="milestone-details-info">
-            <div>
-              <span className="detail-label">Percentage of funds released</span>
-              <span className="funds-required">{this.props.milestone.percentageToUnlock}%</span>
-            </div>
-
-            <div>
-              <span className="detail-label">Funding Released</span>
-              <span className="funds-required">{this.state.formattedMilestoneValue} ${this.props.projectOnChain.currencyId as Currency}</span>
-            </div>
-
+  return (
+    <div className="main">
+      <div
+        className="milestone-row"
+        onClick={() => {
+          toggleMilestone(milestone.milestoneKey);
+        }}
+      >
+        <span>{milestone.name}</span>
+        {displayMilestoneToggle()}
+      </div>
+      <div className={toggleActive ? "content show" : "content hide"}>
+        <div className="milestone-details-info">
+          <div>
+            <span className="detail-label">Percentage of funds released</span>
+            <span className="funds-required">{milestone.percentageToUnlock}%</span>
           </div>
+
+          <div>
+            <span className="detail-label">Funding Released</span>
+            <span className="funds-required">{formattedMilestoneValue} ${projectOnChain.currencyId as Currency}</span>
+          </div>
+
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MilestoneItem;

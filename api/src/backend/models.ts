0d428986e5c172abb2ca587648eaeb830342549e
sam elamin <hussam.elamin@gmail.com>
2023-02-15 19:22:49 +0000

    fixes for brief details

--- api/src/backend/models.ts
+++ api/src/backend/models.ts
@@ -330,6 +330,7 @@ export const fetchAllBriefs = () =>
                 "users.display_name as created_by",
                 "experience_level",
                 "briefs.experience_id",
+                "briefs.created",
                 "users.briefs_submitted as number_of_briefs_submitted",
                 tx.raw("ARRAY_AGG(DISTINCT CAST(skills.name as text)) as skills"),
                 tx.raw("ARRAY_AGG(DISTINCT CAST(industries.name as text)) as industries"),

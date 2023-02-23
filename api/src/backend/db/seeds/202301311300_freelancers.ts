0693774f2be3d5bbbea0ac2ec01f65c6a822d6a2
felix <felixgate13@gmail.com>
2023-02-23 14:57:00 +0000

    only gen a few passwords in seed

--- api/src/backend/db/seeds/202301311300_freelancers.ts
+++ api/src/backend/db/seeds/202301311300_freelancers.ts
@@ -47,15 +47,13 @@ export async function seed(knex: Knex): Promise<void> {
                     let rsmall =  Math.floor(Math.random() * (5));
                     let rbig =  Math.floor(Math.random() * (100000));
 
-                    const salt = bcrypt.genSaltSync(10);
-
                     // new user associated
                     await knex("users").insert(
                         [{
                             display_name: skills[a] + "_" + clients[b] + "_" + services[d] + rbig,
                             username: skills[a].replace(" ","_") + "_" + clients[b].replace(" ","_") + "_" + services[d].replace(" ","_") + rbig,
                             email: skills[a] + languages[c] + rbig + "@gmail.com",
-                            password: bcrypt.hashSync("testpassword", salt),
+                            password:  a < 2 ? bcrypt.hashSync("testpassword") : "testpassword"  
                         }]
                      ).then(async () => {
                          await knex("freelancers").insert({

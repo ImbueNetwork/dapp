8c8369acb7841b6abb30e5a7a45f55aed70694cc
samelamin <hussam.elamin@gmail.com>
2023-03-14 14:37:08 +0000

    add project id to brief

--- api/src/backend/db/migrations/202210061410_create_brief.ts
+++ api/src/backend/db/migrations/202210061410_create_brief.ts
@@ -13,6 +13,8 @@ export async function up(knex: Knex): Promise<void> {
         builder.integer("duration_id");
         builder.bigInteger("budget");
         builder.integer("experience_id");
+        builder.integer("project_id");
+        builder.foreign("project_id").references("projects.id");
 
         // stored in its own table
         // The foreign key is put on in the experience migration.

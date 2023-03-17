d4f2bfe3716f7f963ba00c4a678a8153e79eb3ae
samelamin <hussam.elamin@gmail.com>
2023-03-17 19:17:23 +0000

    channel list design

--- api/src/frontend/components/dashboard-chat.tsx
+++ api/src/frontend/components/dashboard-chat.tsx
@@ -6,6 +6,10 @@ import { StreamChat } from 'stream-chat';
 import { Chat, Channel, ChannelList, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 import 'stream-chat-react/dist/css/v2/index.css';
 import { getStreamChat } from "../utils";
+import '../Styles/dashboard.css'
+
+import EditIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import { TextField } from "@mui/material";
 
 export type DashboardProps = {
     user: User;
@@ -34,17 +38,30 @@ export const DashboardChat = ({ user }: DashboardProps): JSX.Element => {
 
     return (
         client ?
-        <Chat client={client} theme='str-chat__theme-dark'>
-            <ChannelList filters={filters} />
-            <Channel >
-                <Window>
-                    <ChannelHeader />
-                    <MessageList />
-                    <MessageInput />
-                </Window>
-                <Thread />
-            </Channel>
-        </Chat>
+        <div className="custom-chat-container w-full rounded-2xl border border-white overflow-hidden">
+            <Chat client={client} theme='str-chat__theme-dark'>
+                <div className="flex h-full">
+                    <div className="border-r border-r-white">
+                        <div className="flex px-12 pt-6 justify-between items-center">
+                            <h2 className="font-extrabold flex">Messaging</h2>                    
+                            <EditIcon/>
+                        </div>
+                        <ChannelList 
+                        filters={filters} 
+                        showChannelSearch={true}/>
+                    </div>
+
+                <Channel >
+                    <Window>
+                        <ChannelHeader />
+                        <MessageList />
+                        <MessageInput />
+                    </Window>
+                    <Thread />
+                </Channel>
+                </div>
+            </Chat>
+        </div>
         : <p>REACT_APP_GETSTREAM_API_KEY not found</p>
     );
 };

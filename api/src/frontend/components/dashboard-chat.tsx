4e709300e5d07bafa771585e5e49832df5146c66
ssani7 <sanaullah.sani756@gmail.com>
2023-03-26 18:56:15 +0600

    created nav bar

--- api/src/frontend/components/dashboard-chat.tsx
+++ api/src/frontend/components/dashboard-chat.tsx
@@ -22,7 +22,11 @@ import { getStreamChat } from "../utils";
 import "../Styles/dashboard.css";
 
 import EditIcon from "@mui/icons-material/ModeEditOutlineOutlined";
-import { TextField } from "@mui/material";
+import { BottomNavigation, BottomNavigationAction, TextField } from "@mui/material";
+
+const navBarStyles = {
+
+}
 
 export type DashboardProps = {
     user: User;
@@ -31,7 +35,7 @@ export type DashboardProps = {
 export const DashboardChat = ({ user }: DashboardProps): JSX.Element => {
     const [client, setClient] = useState<StreamChat>();
     const filters = { members: { $in: [user.username] } };
-    const active = true;
+    const [value, setValue] = useState<number>(2)
 
     useEffect(() => {
         async function setup() {
@@ -52,7 +56,7 @@ export const DashboardChat = ({ user }: DashboardProps): JSX.Element => {
 
     const CustomChannelHeader = (props: ChannelHeaderProps) => {
         const { title } = props;
-        const { channel, members,watcher_count } = useChannelStateContext();
+        const { channel, members, watcher_count } = useChannelStateContext();
         const { name, image } = channel?.data || {};
 
         return (
@@ -65,7 +69,7 @@ export const DashboardChat = ({ user }: DashboardProps): JSX.Element => {
                                 className="w-16 h-16 rounded-full object-cover object-top"
                                 alt=""
                             />
-                            {watcher_count && watcher_count >=2 && <div className="h-4 w-4 bg-green-500 rounded-full absolute bottom-0 right-0 border-2 border-black"></div>}
+                            {watcher_count && watcher_count >= 2 && <div className="h-4 w-4 bg-green-500 rounded-full absolute bottom-0 right-0 border-2 border-black"></div>}
                         </div>
                     )}
                     <div className="flex flex-col items-start">
@@ -84,27 +88,47 @@ export const DashboardChat = ({ user }: DashboardProps): JSX.Element => {
     };
 
     return client ? (
-        <div className="custom-chat-container w-full rounded-2xl border border-white overflow-hidden">
-            <Chat client={client} theme="str-chat__theme-dark">
-                <div className="flex h-full">
-                    <div className="chat-list-container border-r border-r-white">
-                        <ChannelList
-                            filters={filters}
-                            showChannelSearch={true}
-                        />
-                    </div>
+        <div className="-mt-6">
+            <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                sx={{
+                    bgcolor: "#2c2c2c",
+                    borderRadius:"20px",
+                    border: "1px solid white",
+                    marginBottom: '36px',
+                    overflow:"hidden"
+                }}
+            >
+                <BottomNavigationAction label="Client View" value={1} />
+                <BottomNavigationAction label="Messages" value={2} />
+                <BottomNavigationAction label="Freelancer View" value={3} />
+            </BottomNavigation>
+            <div className="custom-chat-container w-full rounded-2xl border border-white overflow-hidden">
+                <Chat client={client} theme="str-chat__theme-dark">
+                    <div className="flex h-full">
+                        <div className="chat-list-container border-r border-r-white">
+                            <ChannelList
+                                filters={filters}
+                                showChannelSearch={true}
+                            />
+                        </div>
 
-                    <Channel>
-                        <Window>
-                            {/* <ChannelHeader /> */}
-                            <CustomChannelHeader />
-                            <MessageList />
-                            <MessageInput />
-                        </Window>
-                        <Thread />
-                    </Channel>
-                </div>
-            </Chat>
+                        <Channel>
+                            <Window>
+                                {/* <ChannelHeader /> */}
+                                <CustomChannelHeader />
+                                <MessageList />
+                                <MessageInput />
+                            </Window>
+                            <Thread />
+                        </Channel>
+                    </div>
+                </Chat>
+            </div>
         </div>
     ) : (
         <p>REACT_APP_GETSTREAM_API_KEY not found</p>
